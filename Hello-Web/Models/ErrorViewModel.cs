c4b5cfc80085e4ad4239301d13baa9e93015c09e
Michael Minto <michael.minto@orica.com>
2018-05-29 13:04:20 +1000

    Add project files.

---
+++ Hello-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hello_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
